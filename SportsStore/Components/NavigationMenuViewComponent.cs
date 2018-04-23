77773e0acc8acfe69abb32c726cfba0ac4fe6c68
mareveles <mareveles1@buffs.wtamu.edu>
2018-04-22 19:38:02 -0500

    Finished Navigation Component

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components{
+    public class NavigationMenuViewComponent : ViewComponent {
+        public string Invoke() {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -12,9 +12,10 @@ namespace SportsStore.Controllers{
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 1) 
+        public ViewResult List(string category,int productPage = 1) 
         => View(new ProductsListViewModel{
             Products = repository.Products
+            .Where(p => category == null || p.Category == category)
             .OrderBy(p => p.ProductID)
             .Skip((productPage-1)*PageSize)
             .Take(PageSize),
@@ -22,7 +23,8 @@ namespace SportsStore.Controllers{
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
                 TotalItems = repository.Products.Count()
-            }
+            }, 
+            CurrentCategory = category
         });
     }
 }
